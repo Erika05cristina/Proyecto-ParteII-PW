94936ae1e315c48d0fdf2e10d777277bfc20e7bf
Jonnathan23 <jsaquicela26@gmail.com>
2024-07-16 13:01:21 -0500

    Gestion de usuario y libros añadido

---
+++ proyectobackend/src/main/java/Gestions/GestionBook.java
@@ -0,0 +1,43 @@
+package Gestions;
+
+import java.util.List;
+
+import Dao.BookDao;
+import Model.Book;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class GestionBook {
+	@Inject
+	private BookDao bookDao;
+	
+	public Book getBook(int id) throws Exception {
+		Book bookFound = this.bookDao.searchBook(id);
+		
+		if(bookFound == null) throw new Exception("Libro no existente");
+		
+		return bookFound;
+	}
+	
+	public void createBook(Book book) throws Exception{
+		if (book == null) throw new Exception ("Libro vacio");
+		
+		this.bookDao.createBook(book);
+	}
+	
+	public void updateBook(Book book) throws Exception {
+		if (book == null) throw new Exception ("Libro vacio");
+		
+		this.bookDao.updateBook(book);
+	}
+	
+	public List<Book> listBooks(){
+		return this.bookDao.listBooks();
+	}
+	
+	public List<Book> listBooksCategory(String category){
+		return this.bookDao.listBooksCategories(category);
+	}
+	
+}
---
+++ proyectobackend/src/main/java/Gestions/GestionUser.java
@@ -0,0 +1,42 @@
+package Gestions;
+
+import Dao.UserDao;
+import Model.User;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class GestionUser {
+
+	@Inject
+	private UserDao userDao;
+	
+	public void createUser(User user) throws Exception{
+		if(user == null) throw new Exception ("Error al crear el usuario");
+		
+		this.userDao.createUser(user);
+	}
+	
+	public void updateUser(User user) throws Exception {
+		if(user == null) throw new Exception ("Error al modificar el usuario");
+		
+		this.userDao.updateUser(user);		
+	}
+	
+	public User searchEmailUser(String email) throws Exception {
+		User userFound = this.userDao.searchEmailUser(email);
+		
+		if(userFound == null) throw new Exception ("Usuario no encontrado");
+		
+		return userFound;
+	}
+	
+	public User searchUser(int id) throws Exception {
+		User userFound = this.userDao.searchIdUser(id);
+		
+		if(userFound == null) throw new Exception ("Usuario no encontrado");
+		
+		return userFound;
+	}
+	
+}
