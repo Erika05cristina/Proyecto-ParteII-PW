f2ab6726e749e92435a0783b8df1b0101566c86a
Jonnathan23 <jsaquicela26@gmail.com>
2024-07-24 17:54:31 -0500

    Encabezado modificado para dar permisos del cors

--- proyectobackend/src/main/java/CorsFilter.java
+++ proyectobackend/src/main/java/CorsFilter.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
@@ -6,23 +9,39 @@ import jakarta.ws.rs.ext.Provider;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
+	
+	/** @description Lista de las url permitidas por nuestra api*/
+	private static final List<String> ALLOWED_ORIGINS = Arrays.asList(
+			"http://localhost:4200",
+			"http://localhost:58184"
+			
+			);
 
-    @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-        // Agregar encabezados CORS
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        responseContext.getHeaders().add("Access-Control-Max-Age", "3600");  // Cache preflight response
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
 
-        
-        // Log para verificar que los encabezados se están añadiendo
-        System.out.println("CORS headers added");
-        System.out.println("Access-Control-Allow-Origin: " + responseContext.getHeaders().getFirst("Access-Control-Allow-Origin"));
-        System.out.println("Access-Control-Allow-Credentials: " + responseContext.getHeaders().getFirst("Access-Control-Allow-Credentials"));
-        System.out.println("Access-Control-Allow-Headers: " + responseContext.getHeaders().getFirst("Access-Control-Allow-Headers"));
-        System.out.println("Access-Control-Allow-Methods: " + responseContext.getHeaders().getFirst("Access-Control-Allow-Methods"));
-    }
+		
+		String origin = requestContext.getHeaderString("Origin");
+		if (ALLOWED_ORIGINS.contains(origin)) {
+			// Agregar encabezados CORS
+			responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+			responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+			responseContext.getHeaders().add("Access-Control-Allow-Headers",
+					"origin, content-type, accept, authorization");
+			responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+			responseContext.getHeaders().add("Access-Control-Max-Age", "3600"); // Cache preflight response
+
+			// Log para verificar que los encabezados se están añadiendo
+			System.out.println("CORS headers added");
+			System.out.println("Access-Control-Allow-Origin: "
+					+ responseContext.getHeaders().getFirst("Access-Control-Allow-Origin"));
+			System.out.println("Access-Control-Allow-Credentials: "
+					+ responseContext.getHeaders().getFirst("Access-Control-Allow-Credentials"));
+			System.out.println("Access-Control-Allow-Headers: "
+					+ responseContext.getHeaders().getFirst("Access-Control-Allow-Headers"));
+			System.out.println("Access-Control-Allow-Methods: "
+					+ responseContext.getHeaders().getFirst("Access-Control-Allow-Methods"));
+		}
+	}
 }
